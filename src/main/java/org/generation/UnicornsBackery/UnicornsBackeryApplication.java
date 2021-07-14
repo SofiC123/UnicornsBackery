1c4ccbcede49749160884177a17338359ecdc381
Armando <armando.espinozza@gmail.com>
2021-07-14 18:12:56 -0500

    Tercer comentario de prueba

--- src/main/java/org/generation/UnicornsBackery/UnicornsBackeryApplication.java
+++ src/main/java/org/generation/UnicornsBackery/UnicornsBackeryApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UnicornsBackeryApplication {
 
 	public static void main(String[] args) {
-		//Comentario de prueba , segunda prueba ramon
+		//Comentario de prueba , segunda prueba ramon, tercera prueba Mario
 		SpringApplication.run(UnicornsBackeryApplication.class, args);
 	}
 
