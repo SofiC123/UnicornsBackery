005c2418aee58f6da13b86a5687a93b181a5909d
SofiaC123 <lorenasofia.cayeros@gmail.com>
2021-07-14 17:40:20 -0500

    Primer commit: inicial.

---
+++ src/main/java/org/generation/UnicornsBackery/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.generation.UnicornsBackery;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(UnicornsBackeryApplication.class);
+	}
+
+}
---
+++ src/main/java/org/generation/UnicornsBackery/UnicornsBackeryApplication.java
@@ -0,0 +1,13 @@
+package org.generation.UnicornsBackery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicornsBackeryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnicornsBackeryApplication.class, args);
+	}
+
+}
