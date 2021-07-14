3709b6a973ae6905e23c4124db91920a06363f7b
OscarValG <72753785+OscarValG@users.noreply.github.com>
2021-07-14 17:44:40 -0500

    Comentario de prueba

--- src/main/java/org/generation/UnicornsBackery/UnicornsBackeryApplication.java
+++ src/main/java/org/generation/UnicornsBackery/UnicornsBackeryApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UnicornsBackeryApplication {
 
 	public static void main(String[] args) {
+		//Comentario de prueba
 		SpringApplication.run(UnicornsBackeryApplication.class, args);
 	}
 
